f79ad39c5b1b07e0d33f5d342ca024c044fa7758
dedinkorea <dedinkorea@gmail.com>
2025-12-03 18:52:06 +0900

    Delete artifacts directory

--- artifacts/malicious.js
+++
@@ -1,7 +0,0 @@
-// malicious.js
-// Извлечено из HTTP-ответа yummyrecipesforme.com (200 OK)
-// Wireshark → Follow → HTTP stream → Save as...
-
-setTimeout(function(){
-    location.href = "http://greatrecipesforme.com/";
-}, 5000);
